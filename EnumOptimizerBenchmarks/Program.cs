3ce5372adea723f11fbe9255ab1674d10b731e57
korov <korova2321@gmail.com>
2021-08-21 13:48:21 +0300

    Created benchmarks for fast to string method

---
+++ EnumOptimizerBenchmarks/Program.cs
@@ -0,0 +1,37 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using EnumOptimizer;
+using System;
+
+namespace EnumOptimizerBenchmarks
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<Benchmark>();
+        }
+    }
+
+    public enum MediaType
+    {
+        Video, Image, Album, LiveStream
+    }
+
+    [MemoryDiagnoser]
+    public class Benchmark
+    {
+        private MediaType Type = MediaType.Video;
+        [Benchmark]
+        public string NativeToString()
+        {
+            return Type.ToString();
+        }
+
+        [Benchmark]
+        public string FastToString()
+        {
+            return Type.FastToString();
+        }
+    }
+}
